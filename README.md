303088214f4ad34a8b16b077178086f2210f1e64
Muhammad Ahsan <53137713+ahsansoftengineer@users.noreply.github.com>
2024-06-06 18:16:15 +0500

    FIXED

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
-https://dev-app.starbazaar.pk/admin/api/v1/brands-payment-security?
 
-access Token
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiIxNjU1NDcwNjEyOTY3IiwiaWF0IjoxNzE3NjY4NDMyfQ.jk6eMBujvFv50fhYaUghj9IXekHA9pysC5aoShtRy4M"
 
--- src/app/shared/interceptors/auth.interceptor.ts
+++ src/app/shared/interceptors/auth.interceptor.ts
@@ -2,7 +2,7 @@ import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/c
 import { Observable } from 'rxjs';
 
 export class AuthInterceptor implements HttpInterceptor {
-  private token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiIxNzE0MTE4MzM5NDQ0IiwiaWF0IjoxNzE1MTYwNzIxfQ.JI-5nv7IZscuvrVJr6QiLOKbJ9k17fZdPR31JdSVaAY"
+  private token = "token replace"
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const modifiedRequest = req.clone({setHeaders: {Authorization: `Bearer ${this.token}`}})
     return next.handle(modifiedRequest)
